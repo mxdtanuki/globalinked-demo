aee29cb0f31385553bedfbd1327b5548413a299b
getaway car <mikyllasamanthampalaming@iskolarngbayan.pup.edu.ph>
2025-08-09 02:58:11 +0800

    font-end public page

---
+++ frontend/public-page/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div>
+          <h3>Office of International Affairs</h3>
+          <p>Connecting our university to the world</p>
+          <p>through education, research,</p>
+          <p>and cultural exchange.</p>
+        </div>
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li><a href="#">Memorandum Of Agreements/Memorandum of Understanding</a></li>
+            <li><a href="#">Objective and Functions</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Frequently Asked Questions</a></li>
+            <li><a href="#">Officials and Staff</a></li>
+          </ul>
+        </div>
+        <div>
+          <h4>Services</h4>
+          <ul>
+            <li>International Exchange Students</li>
+            <li>International Seminars/Fora</li>
+            <li>International Scholarship Grants</li>
+            <li>International Faculty Exchange</li>
+            <li>International Organizations Affiliation</li>
+            <li>International Linkages</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
